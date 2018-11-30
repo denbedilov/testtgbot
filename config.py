641648216950febb5a976efda7bcd38aa2d2417d
denys <denbedilov@gmail.com>
2018-11-30 18:37:48 +0200

    config file

---
+++ config.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+token = '726657987:AAH05wsRacc3wyr07CkrffcMXgJrCCmMvBs'
+
+users = ['@denysko','@kovalskyii','@siamskiykot','@khash_ty','@Mr_Pengu1n','@Wulwsten','@Blade4ever','@pomidorka_pewpew','@Piksh','@FatHippie']
